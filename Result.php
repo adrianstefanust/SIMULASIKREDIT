d556873c5e350571589ebb7eec5b37863f4fc49f
unknown <astefanus44@gmail.com>
2019-02-12 03:31:29 +0700

    init file

---
+++ Result.php
@@ -0,0 +1,52 @@
+<!doctype HTML>
+<head>
+  <title>HASIL SIMULASI KALKULATOR KREDIT</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<h3 class="text-center">HASIL PERHITUNGAN KALKULASI KREDIT</h3>
+<hr>
+<div class="container">
+<table class="table table-striped">
+<thead class="thead-dark">
+<tr>
+    <th >Periode</th>
+    <th >Angsuran</th>
+    <th >Biaya Admin</th>
+    <th >Bunga</th>
+    <th >Total Angsuran</th>
+<?php
+    $jml_pinjaman = $_GET['jumlah_pinjaman'];
+    $sk_bunga = $_GET['sk_bunga'];
+    $tenor = $_GET['tenor'];
+    $biaya_admin = $_GET['biaya'];
+    $angsuran = $jml_pinjaman/$tenor;
+    $total = 0;
+    for ($temp = 1; $temp <= $tenor; $temp++) {
+        
+        $bunga = ($jml_pinjaman - (($temp-1)*$angsuran)) * ($sk_bunga/100) / 12;
+        $total_angsuran = $angsuran + $bunga + $biaya_admin;
+        $total +=  $total_angsuran
+        ?>
+        <tr>
+            <td ><?php echo $temp;?></td>
+            <td><?php echo number_format($angsuran,2) ;?></td>
+            <td><?php echo number_format($biaya_admin,2);?></td>
+            <td><?php echo number_format($bunga,2);?></td>
+            <td><?php echo number_format($total_angsuran,2);?></td>
+        </tr>
+        <?php
+    } 
+?>
+<tr>
+</thead>
+    <td colspan="4" ><h4>TOTAL KALKULASI : </h4></td>
+    <td colspan="3" ><?php echo number_format($total,2);?></td>
+</tr>
+</tr>
+</table>
+</div>
+</html>
